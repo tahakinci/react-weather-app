9b17902d5debabbb5024a4e92d3e981d3f2e84a2
Muhammed Taha Akinci <106470797+tahakinci@users.noreply.github.com>
2023-05-31 16:14:29 +0300

    i will delete this

--- src/components/SearchParams.jsx
+++ src/components/SearchParams.jsx
@@ -64,6 +64,7 @@ const SearchParams = () => {
           otherCitiesData={cities}
         />
       ) : null}
+      <SearchButton setSearch={handleSearch} handleSubmit={handleSubmit} />
     </div>
   );
 };
